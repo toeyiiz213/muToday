81bff97b0d7194ef6651f6768ddfb630a80b6867
toeyiiz213 <duckkiegander@gmail.com>
2025-06-06 11:51:21 +0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,12 +2,12 @@ from databricks.sql import connect
 print("Start")
 try:
     print("Start Connect")
-    acc = "dapie20a5689668815dc5b6f60ff21dcf233"
-    connection = connect(
-        server_hostname="886789292378781.1.gcp.databricks.com",  # ลบ https:// ออก
-        http_path="/sql/1.0/warehouses/9a6582c8c1fe8f71",
-        access_token=acc
-    )
+    # acc = "dapie20a5689668815dc5b6f60ff21dcf233"
+    # connection = connect(
+    #     server_hostname="886789292378781.1.gcp.databricks.com",  # ลบ https:// ออก
+    #     http_path="/sql/1.0/warehouses/9a6582c8c1fe8f71",
+    #     access_token=acc
+    # )
     print("After Connect")
     cursor = connection.cursor()
     cursor.execute("SELECT current_date()")
